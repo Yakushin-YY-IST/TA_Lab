10124ba402403b287629c7050c582629fc698d58
Yakushin-YY-IST <antihrist.immortal@gmail.com>
2017-04-17 20:23:56 +0300

    Доработать ЛР 5

---
+++ TeorAlg-lab1/TeorAlg-lab1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeorAlg_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double z1, z2, a=0;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите а=");
+                    a = Convert.ToDouble(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Неверный формат ввода!");
+                    continue;
+                }
+                break;
+            }
+            z1 = 5 - 2 * Math.Pow(a,2);
+            z1 /= (1 + a + Math.Pow(a, 2)) / (2 * a + Math.Pow(a, 2)) + 2 - (1 - a + Math.Pow(a, 2)) / (2 * a - Math.Pow(a, 2));
+            Console.WriteLine("z1=" + z1);
+            z2 = (4 - Math.Pow(a, 2)) / 2;
+            Console.WriteLine("z2=" + z2);
+            if (z1 == z2)
+                Console.WriteLine("z1=z2");
+            Console.WriteLine("Для выхода нажми что-нибудь");
+            Console.ReadKey(true);
+        }
+    }
+}
+
+
+
---
+++ TeorArg_lab5_var19/TeorArg_lab5_var19/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeorArg_lab5_var19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double MAX = 10, MIN = -5, imul_neg=1, sum_pos=0; //sum_pos сумма всех положительных
+            Int32 long_array = 10;
+            double[] array = new double[long_array];
+            for (int i = 0; i < long_array; i++)
+            {
+                array[i] = Rand(MAX,MIN);
+            }
+            Double max_add = MIN;
+            for (int i=0; i<long_array;i++)
+            {
+                Console.WriteLine("Элемент массива "+i+"\tЗначение: "+array[i]);
+                if (array[i] < 0) imul_neg = imul_neg * array[i];
+                else sum_pos = sum_pos + array[i];
+                if (max_add < array[i]) max_add = array[i];
+            }
+            if (imul_neg == 1) Console.WriteLine("Отрицательных элементов в массиве нет");
+            else Console.WriteLine("Произведение отрицательных = ");
+            Console.WriteLine("Сумма положительных (без максимального элемента) = " + (sum_pos - max_add));
+            Console.ReadKey(true);
+        }
+        public static Double Rand(Double max, Double min)
+        {
+            Random rand = new Random();
+            return Math.Round(rand.NextDouble() * max - rand.NextDouble() * min, 2);
+        }
+    }
+}
